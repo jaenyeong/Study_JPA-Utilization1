4241f5536f0d9e6be88823e6326654984911b9d8
jaenyeong <jaenyeong.dev@gmail.com>
2021-04-08 21:23:54 +0900

    feat(ItemService at el): Add item service & repository

---
+++ src/main/java/com/jaenyeong/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,33 @@
+package com.jaenyeong.jpabook.jpashop.repository;
+
+import com.jaenyeong.jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(final Item item) {
+        if (item.getId() == null) {
+            em.persist(item);
+            return;
+        }
+
+        em.merge(item);
+    }
+
+    public Item findOne(final Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+            .getResultList();
+    }
+}
