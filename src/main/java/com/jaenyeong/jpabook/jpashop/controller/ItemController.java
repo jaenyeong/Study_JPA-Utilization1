4774d26c3ea03666d992565564a98d3e8b5269a2
jaenyeong <jaenyeong.dev@gmail.com>
2021-04-09 12:12:54 +0900

    feat(createItemForm at el): Add item add feature

---
+++ src/main/java/com/jaenyeong/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,36 @@
+package com.jaenyeong.jpabook.jpashop.controller;
+
+import com.jaenyeong.jpabook.jpashop.domain.item.Book;
+import com.jaenyeong.jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(final Model model) {
+        model.addAttribute("itemForm", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(final BookForm bookForm) {
+        final Book book = new Book();
+        book.setName(bookForm.getName());
+        book.setPrice(bookForm.getPrice());
+        book.setStockQuantity(bookForm.getStockQuantity());
+        book.setAuthor(bookForm.getAuthor());
+        book.setIsbn(bookForm.getIsbn());
+
+        itemService.saveItem(book);
+
+        return "redirect:/";
+    }
+}
